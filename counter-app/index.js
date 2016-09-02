38758e75526b7e28081f8397f898636859e036e6
t ddddddd <webdev@cmme.org>
2016-09-02 09:22:15 -0700

    more typical dir structure

---
+++ counter-app/index.js
@@ -0,0 +1,47 @@
+import { Component } from 'panel';
+
+import template from './index.jade';
+import aboutTemplate from './about.jade';
+import counterTemplate from './counter.jade';
+
+document.registerElement('counter-app', class extends Component {
+  get config() {
+    return {
+      defaultState: {
+        $view: 'about',
+        count: 1,
+      },
+
+      routes: {
+        'counter': () => ({$view: 'counter'}),
+        'about':   () => ({$view: 'about'}),
+        '':        'about',
+      },
+
+      template,
+    };
+  }
+});
+
+document.registerElement('about-view', class extends Component {
+  get config() {
+    return {template: aboutTemplate};
+  }
+});
+
+document.registerElement('counter-view', class extends Component {
+  get config() {
+    return {
+      helpers: {
+        decr: () => this.changeCounter(-1),
+        incr: () => this.changeCounter(1),
+      },
+
+      template: counterTemplate,
+    };
+  }
+
+  changeCounter(offset) {
+    this.update({count: this.state.count + offset});
+  }
+});
--- index.js
+++ index.js
@@ -1,52 +1,8 @@
-/* polyfills for older browsers, always import first */
+/* --- polyfills for older browsers, always import first --- */
+
 import 'babel-polyfill';
 import 'webcomponents.js';
-/* end polyfills */
 
-import { Component } from 'panel';
+/* --- end polyfills --- */
 
-import template from './app.jade';
-import aboutTemplate from './about.jade';
-import counterTemplate from './counter.jade';
-
-document.registerElement('counter-app', class extends Component {
-  get config() {
-    return {
-      defaultState: {
-        $view: 'about',
-        count: 1,
-      },
-
-      routes: {
-        'counter': () => ({$view: 'counter'}),
-        'about':   () => ({$view: 'about'}),
-        '':        'about',
-      },
-
-      template,
-    };
-  }
-});
-
-document.registerElement('about-view', class extends Component {
-  get config() {
-    return {template: aboutTemplate};
-  }
-});
-
-document.registerElement('counter-view', class extends Component {
-  get config() {
-    return {
-      helpers: {
-        decr: () => this.changeCounter(-1),
-        incr: () => this.changeCounter(1),
-      },
-
-      template: counterTemplate,
-    };
-  }
-
-  changeCounter(offset) {
-    this.update({count: this.state.count + offset});
-  }
-});
+import './counter-app';
